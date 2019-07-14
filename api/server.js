6405234f8d0b80e0b9474e5e2b6f1e649bd50a8b
malarc01 <email@example.com>
2019-07-14 14:30:02 -0400

    npm cors & add recipe router & .env file added PORT=1234

--- api/server.js
+++ api/server.js
@@ -3,6 +3,8 @@ const express = require('express');
 const configureMiddleware = require('./middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const usersRouter = require('../users/users-router.js');
+const recipesRouter = require('../recipes/recipes-router.js')
+
 
 const server = express();
 
@@ -10,6 +12,9 @@ configureMiddleware(server);
 
 server.use('/api/auth', authRouter);
 server.use('/api/users', usersRouter);
+server.use('/api/recipes', recipesRouter);
+
+
 
 server.get('/', (req, res) => {
   res.send("It's alive!");
---
+++ recipes/recipes-router.js
@@ -0,0 +1,73 @@
+const router = require('express').Router();
+
+
+// GET RECIPE table 
+router.get('/', (req, res) => {
+  database('recipes').then(recipe => {
+    res.status(200).json(recipe);
+  }).catch(error => {
+    console.log(error);
+    res.status(500).json(error);
+  })
+})
+
+//POST to RECIPE table
+router.post('/', (req, res) => {
+  database('recipes').insert(req.body, ['id', 'name'])
+    .then(ids => {
+      database('recipes')
+        .where({ id: ids[0] })
+        .first()
+        .then(r => {
+          res.status(200).json(r)
+        })
+    }).catch(error => {
+      res.status(500).json({ error: "POST ERROR!" })
+    })
+})
+
+
+
+// GET RECIPE table with ID
+
+
+
+router.get('/:id', (req, res) => {
+  database('recipes')
+    .where({ id: req.params.id })
+    .first()
+    .then(specificRecipeID => {
+      if (specificRecipeID) {
+        res.status(200).json(specificRecipeID);
+      } else {
+        res.status(404).json({ message: 'RECIPE Id not found' });
+      }
+    })
+    .catch(error => {
+      res.status(500).json(error);
+    })
+})
+
+
+// DEL request to with ID
+router.delete('/:id', (req, res) => {
+  db('recipes')
+    .where({ id: req.params.id })
+    .del()
+    .then(count => {
+      if (count > 0) {
+        res.status(200).json({
+          message: `${count} ${count > 1 ? 'records DELETED' : 'record DELETED'}`
+        })
+
+      } else {
+        res.status(404).json({ message: 'RECIPE does not exist' })
+      }
+
+    }).catch(error => {
+      res.status(500).json(error)
+    })
+
+})
+
+module.exports = router;

f00184458ebae8a98699a825b002fe5df80fcfac
jal9626 <jal9626@gmail.com>
2019-07-18 23:09:40 -0400

    added server GET test and auth POST test

---
+++ auth/auth-router.spec.js
@@ -0,0 +1,32 @@
+const request = require('supertest');
+
+const db = require('../data/dbConfig.js')
+const Users = require('../users/users-model');
+
+describe('the auth-router', () => {
+    
+    describe('POST /register',() => {
+        
+        afterEach(async () => {
+            //clean up
+            await db('recipes').truncate();
+        })
+
+        it('should instert a user into the db', async () => {
+            //using our model method
+            await Users.add({ username: 'Ashley', password: 'abc123'})
+            await Users.add({ username: 'Mandy', password: 'abc123'})
+
+            //confirm with knex
+            const users = await db('users');
+
+            expect(users).toHaveLength(2);
+            expect(users[0].username).toBe('Ashley')
+        })
+    })
+})
+
+
+
+  
+
